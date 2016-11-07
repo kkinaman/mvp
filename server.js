e1b9eadffd9c82b7bd4c2f00c39c330f8c84fb52
Karina Kinaman <kkinaman419@gmail.com>
2016-11-07 12:06:28 -0800

    Refactor for deployment

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.post('/destination/todo/remove', handler.removeTodo);
 
 app.get('/places', handler.fetchPlaces);
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port', port);
 });
